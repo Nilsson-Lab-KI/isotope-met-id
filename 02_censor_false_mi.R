0b34d180acab2061d2825fa9a5b0def5ead44051
Roland Nilsson <roland@minerva.net>
2024-05-30 10:58:33 +0200

    Precalculate MIData object, remove false MI

---
+++ 02_censor_false_mi.R
@@ -0,0 +1,29 @@
+#
+#  Create MI data object and remove false MIs
+#
+
+library(remn)
+
+source("common.R")
+
+# Read HMEC peak areas
+hmec_peak_list <- read_hmec_peak_list()
+hmec_peak_areas <- read_hmec_peak_areas()
+
+# Create MIData object
+hmec_mi_data <- MIData(hmec_peak_areas, exp_columns = 3:length(hmec_peak_areas))
+
+n_peaks <- length(hmec_mi_data$peak_ids)
+n_experients <- length(hmec_mi_data$experiments)
+
+# Remove false MI
+hmec_mi_data <- censor_false_mi(
+    hmec_mi_data,
+    min_experiments = 10
+) 
+
+# Write MIData object
+saveRDS(
+    hmec_mi_data,
+    file.path(mi_data_path, 'hmec_mi_data.rds')
+)
