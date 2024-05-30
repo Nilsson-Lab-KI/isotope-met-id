f62bb6fe8e7367022507b4a109937afe2f2a1fbb
Roland Nilsson <roland@minerva.net>
2024-05-30 11:22:29 +0200

    Fix experiment names (replicates)

--- 02_censor_false_mi.R
+++ 02_censor_false_mi.R
@@ -10,8 +10,17 @@ source("common.R")
 hmec_peak_list <- read_hmec_peak_list()
 hmec_peak_areas <- read_hmec_peak_areas()
 
+experiment_names <- substr(
+    colnames(hmec_peak_areas)[3:length(hmec_peak_areas)],
+    1, 3
+)
+
 # Create MIData object
-hmec_mi_data <- MIData(hmec_peak_areas, exp_columns = 3:length(hmec_peak_areas))
+hmec_mi_data <- MIData(
+    hmec_peak_areas,
+    exp_names = experiment_names,
+    exp_columns = 3:length(hmec_peak_areas)
+)
 
 n_peaks <- length(hmec_mi_data$peak_ids)
 n_experients <- length(hmec_mi_data$experiments)
