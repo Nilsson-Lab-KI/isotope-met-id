69ef65445c627738973cbaac92f8300b44ae0160
Roland Nilsson <roland@minerva.net>
2024-06-14 18:49:37 +0200

    Code for Figure 2

---
+++ figure-2.R
@@ -0,0 +1,195 @@
+#
+#  Code to reproduce plots in Figure 2
+#
+
+#library(igraph)
+
+library(ggplot2)
+library(plotly)
+library(remn)
+
+source("common.R")
+
+# Read data
+hmec_peak_list <- read_hmec_peak_list()
+hmec_peak_areas <- read_hmec_peak_areas()
+
+hmec_mi_data <- readRDS(file.path(mi_data_path, 'hmec_mi_data_censored.rds'))
+n_peaks <- length(hmec_mi_data$peak_ids)
+n_experiments <- length(hmec_mi_data$experiments)
+
+# verify that MIData peak IDs matches peak list
+stopifnot(all(hmec_mi_data$peak_ids == hmec_peak_list$peak_id))
+
+#
+#  Fig 2b 13C enrichment
+#
+
+# 13C enrichment values, peaks x experiments
+
+enrichment_mat <- t(
+    sapply(1:n_peaks,
+        function(p) {
+            apply(
+               get_avg_mid(hmec_mi_data, p, ), 2,
+               isotopic_enrichment
+            )
+        }
+    )
+)
+dimnames(enrichment_mat) <- list(
+    hmec_mi_data$peak_ids,
+    hmec_mi_data$experiments
+)
+
+# number of experiments with labeling for each peak
+table(rowSums(enrichment_mat > 0.05))
+
+# number of metabolite with labeling in > 1 experiment
+sum(rowSums(enrichment_mat > 0.05) > 1)
+
+
+# cluster and plot enrichment matrix
+enrichment_clust <- hclust(
+    as.dist(1 - cor(t(enrichment_mat))),
+    method = "average"
+)
+
+image(
+    t(enrichment_mat[enrichment_clust$order, ]),
+    col = colorRampPalette(c("white", "blue"))(100)
+)
+
+
+#
+# Fig 2c glutathione
+#
+
+# selected experiments to plot
+selected_exp <- c("cys", "glc", "gln", "glu", "gly", "ser")
+
+# 5108 glutathione +H
+mids_gthrd <- get_mid_matrix(hmec_mi_data, "5108", selected_exp)
+plot_mid_matrix(c13correct_cols(mids_gthrd), max_mi_fraction = 0.3)
+
+
+#
+# Fig 2d inosine
+#
+
+# selected experiments to plot
+selected_exp <- c("glc", "gly", "ser")
+
+# 6795 hypoxantine
+mids_hxan <- get_mid_matrix(hmec_mi_data, "6795", selected_exp)
+plot_mid_matrix(c13correct_cols(mids_hxan), max_mi_fraction = 0.3)
+
+# 1679 ribose-5P (pentose)
+mids_pentose <- get_mid_matrix(hmec_mi_data, "1679", selected_exp)
+plot_mid_matrix(c13correct_cols(mids_pentose), max_mi_fraction = 0.3)
+
+# 1467 inosine
+mids_ins <- get_mid_matrix(hmec_mi_data, "1467", selected_exp)
+plot_mid_matrix(c13correct_cols(mids_ins), max_mi_fraction = 0.3)
+
+# convolution hxan x pentose
+mids_conv <- convolute_all(mids_hxan, mids_pentose)
+plot_mid_matrix(c13correct_cols(mids_conv), max_mi_fraction = 0.3)
+
+
+#
+# ED Figure 2b MID distance distribution
+#
+
+# read precomputed distance matrix
+hmec_dm <- readRDS(
+    file.path(mid_distance_path, 'hmec_dm.rds')
+)
+
+# histogram of distances, NA set to maximum
+hist(hmec_dm[lower.tri(hmec_dm)], n = 100)
+
+
+#
+# Figure 2e UMap projection
+#
+
+library(umap)
+
+# set random seed to make plot reproducible
+set.seed(571632932)
+
+umap_config <- umap.defaults
+umap_config$n_neighbors <- 15
+
+umap_proj <- umap(hmec_dm, input = "dist", config = umap_config)
+# make sure peaks map correctly
+stopifnot(all(rownames(umap_proj$layout) == hmec_peak_list$peak_id))
+
+umap_df <- data.frame(
+    peak_id = hmec_peak_list$peak_id,
+    peak_ann = paste(hmec_peak_list$peak_id, hmec_peak_list$netid_annotation),
+    umap_1 = umap_proj$layout[, 1],
+    umap_2 = umap_proj$layout[, 2]
+)
+
+# plot projection
+umap_df %>%
+    ggplot(aes(x = umap_1, y = umap_2)) +
+    geom_point(alpha = 0.7, colour = "black") +
+    theme_classic()
+
+# interactive plot with tooltips
+ggplotly(
+    umap_df %>%
+        ggplot(aes(x = umap_1, y = umap_2, label = peak_id, text = peak_ann)) +
+        geom_point(alpha = 0.7, colour = "black") +
+        geom_text() +
+        theme_classic(),
+    tooltip = "text")
+
+
+#
+# ED Figure 2c  UMap distance vs MID distance
+#
+
+# all pairwise distances, UMap vs MID distance. this generally does not agree well
+umap_dist <- dist(umap_proj$layout)  # pairwise euclidean distances of 2D UMap projection
+local({
+    index <- sample(length(umap_dist), 10000)
+    plot(as.dist(hmec_dm)[index], umap_dist[index], col = alpha("black", 0.4))
+})
+
+# Spearman correlation (lower diagonal)
+cor(as.dist(hmec_dm), umap_dist, method = "spearman")
+
+
+#
+# Figure 2g citryl-glutamate
+#
+
+# selected experiments to plot
+selected_exp <- c("glc", "gln", "glu")
+
+# citrate
+mids_cit <- get_mid_matrix(hmec_mi_data, "1917", selected_exp)
+plot_mid_matrix(c13correct_cols(mids_cit), max_mi_fraction = 0.3)
+
+# glutamate
+mids_glu <- get_mid_matrix(hmec_mi_data, "6683", selected_exp)
+plot_mid_matrix(c13correct_cols(mids_glu), max_mi_fraction = 0.3)
+
+mids_citglu <- get_mid_matrix(hmec_mi_data, "1171", selected_exp)
+plot_mid_matrix(c13correct_cols(mids_citglu), max_mi_fraction = 0.3)
+
+# convolution
+plot_mid_matrix(
+    c13correct_cols(convolute_all(mids_cit, mids_glu)),
+    max_mi_fraction = 0.3)
+
+#
+#  Figure 2h MID and MS2 network -- from cytoscape
+#
+
+
+
